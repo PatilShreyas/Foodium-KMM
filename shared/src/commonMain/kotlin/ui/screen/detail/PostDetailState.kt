4a2b1c3f1f9627319cd04cc899b450010096cd75
Shreyas Patil <shreyaspatilg@gmail.com>
2023-04-30 19:04:05 +0530

    Add post detail state and viewmodel

---
+++ shared/src/commonMain/kotlin/ui/screen/detail/PostDetailState.kt
@@ -0,0 +1,29 @@
+/**
+ * Copyright 2023 Shreyas Patil
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ui.screen.detail
+
+import androidx.compose.runtime.Immutable
+import dev.shreyaspatil.mutekt.core.annotations.GenerateMutableModel
+
+@GenerateMutableModel
+interface PostDetailState {
+    val isLoading: Boolean
+    val post: Post?
+    val errorMessage: String?
+
+    @Immutable
+    data class Post(val title: String, val content: String, val author: String, val imageUrl: String?)
+}
