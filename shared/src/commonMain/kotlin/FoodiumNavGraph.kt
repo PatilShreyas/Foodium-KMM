bf6c5b8ecbb5910da913d88c05df56268af0801d
Shreyas Patil <shreyaspatilg@gmail.com>
2023-04-30 19:05:04 +0530

    Design navigation graph for Foodium app

---
+++ shared/src/commonMain/kotlin/FoodiumNavGraph.kt
@@ -0,0 +1,51 @@
+/**
+ * Copyright 2023 Shreyas Patil
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import androidx.compose.runtime.Composable
+import di.component.AppComponent
+import ui.screen.detail.PostDetailScreen
+import ui.screen.home.HomeScreen
+import utils.navigation.NavHost
+import utils.navigation.OnState
+import utils.navigation.navStackViewModel
+import utils.navigation.rememberNavigationController
+
+@Composable
+fun FoodiumNavGraph() {
+    val navController = rememberNavigationController<Screen>()
+
+    NavHost(navController, initialState = Screen.Home) {
+        OnState<Screen.Home> {
+            val viewModel = it.navStackViewModel {
+                AppComponent.viewModelComponent.provideHomeViewModel(it)
+            }
+
+            HomeScreen(
+                viewModel = viewModel,
+                onNavigateToDetail = { postId -> navController.navigateTo(Screen.Detail(postId)) },
+                it,
+            )
+        }
+
+        OnState<Screen.Detail> {
+            val postId = it.state.postId
+            val viewModel = it.navStackViewModel {
+                AppComponent.viewModelComponent.providePostDetailViewModel(it, postId)
+            }
+
+            PostDetailScreen(viewModel = viewModel, onNavigateUp = navController::navigateUp)
+        }
+    }
+}
