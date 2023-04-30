338299f198c078ef1c85a7a25cb45b2ba106972e
Shreyas Patil <shreyaspatilg@gmail.com>
2023-04-30 18:58:48 +0530

    Add utility for exiting from the app

--- shared/src/iosMain/kotlin/main.ios.kt
+++ shared/src/iosMain/kotlin/main.ios.kt
@@ -13,6 +13,29 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.window.ComposeUIViewController
+import utils.appfinisher.AppFinisher
+import utils.appfinisher.LocalAppFinisher
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController {
+    CompositionLocalProvider(LocalAppFinisher provides iosAppFinisher()) {
+        App()
+    }
+}
+
+fun iosAppFinisher(): AppFinisher {
+    // There is no API provided for gracefully terminating an iOS application.
+    // In iOS, the user presses the Home button to close applications. Should your application have
+    // conditions in which it cannot provide its intended function, the recommended approach is to
+    // display an alert for the user that indicates the nature of the problem and possible actions
+    // the user could take — turning on WiFi, enabling Location Services, etc. Allow the user to
+    // terminate the application at their own discretion.
+    // See this for more info: https://developer.apple.com/library/archive/qa/qa1561/_index.html
+
+    return object : AppFinisher {
+        override fun finish() {
+            // no-op
+        }
+    }
+}
